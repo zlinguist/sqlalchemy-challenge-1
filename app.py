6ee790612dc6741f1af4b90c26760f71d3ca0415
zlinguist <zach.childers@gmail.com>
2020-04-14 19:07:51 -0500

    app.py Update

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
+import datetime as dt
+
 from flask import Flask, jsonify
 
 
@@ -40,7 +42,6 @@ def precipitation():
     session = Session(engine)
 
     last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    import datetime as dt
     last_date_object = dt.datetime.strptime(last_date[0], '%Y-%m-%d').date()
     query_date = last_date_object - dt.timedelta(days=365)
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > query_date)
@@ -55,6 +56,91 @@ def precipitation():
         precip.append(precip_dict)
 
     return jsonify(precip)
+
+
+@app.route("/api/v1.0/stations")
+def stations():
+    session = Session(engine)
+
+    results = session.query(Measurement.station).\
+        group_by(Measurement.station).all()
+
+    session.close()
+
+    stations = []
+
+    for station in results:
+        stations.append(station)
+
+    return jsonify(stations)
+
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    session = Session(engine)
+
+    last_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    last_date_object = dt.datetime.strptime(last_date[0], '%Y-%m-%d').date()
+    query_date = last_date_object - dt.timedelta(days=365)
+
+    most_active = session.query(Measurement.station, func.count(Measurement.station)).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).all()[0][0]
+
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active).\
+        filter(Measurement.date >= query_date)
+
+    session.close()
+
+    tobs_all = []
+    for date, temp in results:
+        tobs_dict = {}
+        tobs_dict["date"] = date
+        tobs_dict["temp"] = temp
+        tobs_all.append(tobs_dict)
+
+    return jsonify(tobs_all)
+
+
+@app_route("/api/v1.0/<start>")
+def start_date(start):
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).all()
+
+    session.close()
+
+    start_only = []
+    for minimum, average, maximum in results:
+        start_dict = {}
+        start_dict["TMIN"] = minimum
+        start_dict["TAVG"] = average
+        start_dict["TMAX"] = maximum
+        start_only.append(start_dict)
+
+    return jsonify(start_only)
+
+
+@app_route("/api/v1.0/<start>/<end>")
+def start_end(start, end):
+    session = Session(engine)
+
+    results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
+        filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+
+    session.close()
+
+    start_end = []
+    for minimum, average, maximum in results:
+        start_dict = {}
+        start_dict["TMIN"] = minimum
+        start_dict["TAVG"] = average
+        start_dict["TMAX"] = maximum
+        start_end.append(start_dict)
+
+    return jsonify(start_end)
 
 
 if __name__ == "__main__":
